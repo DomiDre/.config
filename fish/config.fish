8c2139bae30a9444901822efee2167c56dc0f877
DresenDominique <dominique.dresen@bsi.bund.de>
2025-03-24 12:29:24 +0100

    Add configs for new setup using xfce and i3, add bat for cat, replace alacritty by kitty

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
          
-    if not set -q ZELLIJ
-        if test "$ZELLIJ_AUTO_ATTACH" = "true"
-            zellij attach -c
-        else
-            zellij
-        end
-
-        if test "$ZELLIJ_AUTO_EXIT" = "true"
-            kill $fish_pid
-        end
-    end
+    # if not set -q ZELLIJ
+    #     if test "$ZELLIJ_AUTO_ATTACH" = "true"
+    #         zellij attach -c
+    #     else
+    #         zellij
+    #     end
+    #
+    #     if test "$ZELLIJ_AUTO_EXIT" = "true"
+    #         kill $fish_pid
+    #     end
+    # end
 end
 
 zoxide init fish | source
--- fish/functions/config.fish
+++ fish/functions/config.fish
@@ -1,4 +1,4 @@
-function config --wraps='/usr/bin/git --git-dir=$HOME/configit --work-tree=$HOME' --wraps='/usr/bin/git --git-dir=$HOME/configit --work-tree=$HOME/.config' --description 'alias config=/usr/bin/git --git-dir=$HOME/configit --work-tree=$HOME/.config'
-  /usr/bin/git --git-dir=$HOME/configit --work-tree=$HOME/.config $argv
+function config --wraps='/usr/bin/git --git-dir=$HOME/.configit --work-tree=$HOME/.config' --description 'alias config=/usr/bin/git --git-dir=$HOME/.configit --work-tree=$HOME/.config'
+  /usr/bin/git --git-dir=$HOME/.configit --work-tree=$HOME/.config $argv
         
 end
